9d24ccb3d8f24381a868b3f9c2b203af0f7cbab0
VimanRobert <vimanrobert@yahoo.com>
2024-01-11 20:16:00 +0200

    lab 8

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace Viman_Robert_lab2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
+
+		public IActionResult Chat()
+		{
+			return View();
+		}
+	}
 }
---
+++ Views/Books/Chat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Viman_Robert_lab2.Views.Books
+{
+    public class ChatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
